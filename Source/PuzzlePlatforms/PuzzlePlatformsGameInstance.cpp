595a660b33c29058e8a44ef3aeee3973e7639a2d
MeisterTJ <chogdak@gmail.com>
2021-01-31 23:38:40 +0900

    - 서버 호스트 명령어 추가, 클라이언트 조인 명령어 추가 - 서버에서 Host 시 ServerTravel로 Lobby -> Ingame으로 이동 - 클라이언트에서 Join시 Server IP로 조인

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -2,4 +2,45 @@
 
 
 #include "PuzzlePlatformsGameInstance.h"
+#include "Engine/Engine.h"
 
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	// 2초 동안 초록색으로 Hosting 텍스트를 띄운다. 
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	// Jumps the server to new level, If bAbsoulte is true and we are using seemless traveling
+	// listen으로 다른 클라이언트를 받을 수 있도록 열어둔다. (안열면 다른 클라이언트에서 접속 불가)
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+}
+
+// 클라이언트 -> 리스닝하고 있는 서버로 접속한다.
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	// Address로 서버에 접속 및 서버의 레벨로 이동?
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
